9dda57281aca90ffbb430229d483728827187683
bbbbbge <3101663227@qq.com>
2021-04-03 23:11:37 +0800

    4.3日学习代码

---
+++ test_4_3/test_4_3.1/test_4_3.1/C_test_1.c
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <stdio.h>
+//#define MAX 5+5
+//int main()
+//{
+//	printf("%d\n", 3 * MAX);
+//	printf("%d\n", 3 * (MAX));
+//	return 0;
+//}
+//#include <stdio.h>
+//int get_max(int a, int b)
+//{
+//	if (a > b)
+//		return a;
+//	else
+//		return b;
+//}
+//int main()
+//{
+//	int a, b;
+//	int MAX = 0;
+//	scanf("%d %d", &a, &b);
+//	MAX = get_max(a, b);
+//	printf("%d\n", MAX);
+//	return 0;
+//}
+//#include <stdio.h>
+//
+//int main()
+//{
+//	/*int arr[10] = { 0 };
+//
+//	int num = 10;
+//	int arr[num] = { 0 };
+//
+//	const int num = 10;
+//	int arr[num] = { 10 };*/
+//
+//	return 0;
+//}
+//#include <stdio.h>
+//#define Add(a,b) a+b
+//int main()
+//{
+//	int x = 2;
+//	int y = 3;
+//	int sum = Add(x,y);
+//	printf("%d\n",sum);
+//	return 0;
+//}
+#include <stdio.h>
+//enum Day {
+//	Mon,Tue,Wed,Thus,Fri,Sat,Sun
+//}Workday;
+//int main()
+//{
+//	printf("")
+//}
+//int main()
+//{
+//	int a = 10;
+//	float f = 10.0;
+//
+//	return 0;
+//}
