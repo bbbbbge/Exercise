7210a4c2970ae05064036e310bd529adbb9b8e23
bbbbbge <3101663227@qq.com>
2021-04-04 22:17:52 +0800

    python第一次学习代码

---
+++ python_4_4/初识python.py
@@ -0,0 +1,26 @@
+# a,b,c,d=10,5,0,3
+#print(a or b)
+#逻辑运算符 and or not------用bool类型比较
+
+#print(a/d)
+#优先级：()>not>and>or
+
+#a**=b---->a=a^b
+#输出-------%占位符
+#print---自带换行----\n也是换行
+# name='憨批蛋'
+# age=18
+# print('我是%s呀,今年%d岁呀'%(name,age))
+# name='小龙王'
+# QQ='520'
+# print('我还是%s啊，QQ<%s>呐'%(name,QQ))
+# #输出的其他方式
+# name='小蛤蟆'
+# describle='有肉腿还白'
+# print('我小名叫{},{}'.format(name,describle))
+
+#input接受的键盘输入结果都是str类型的，如果接收数字类型需要将str转成int
+# name=input("请输入姓名：")
+# # # # age=float(input("请输入年龄："))
+# # # # #print('我叫{},今年{}岁'.format(name,age))
+# # # # print('我叫%s，今年%f岁'%(name,age))
