dd5168fd3d5c0fd14c8291fa051c08a08ca2afd9
bbbbbge <3101663227@qq.com>
2021-04-09 23:48:55 +0800

    C++顺序结构练习

---
+++ C++_4_9/C++入门及简单顺序结构.cpp
@@ -0,0 +1,185 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a,b;
+//	cin>>a>>b;
+//	cout<<a+b<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	cout<<"Hello World!"<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	bool a=8;
+//	cout<<a<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//
+//int main()
+//{
+//	string str;
+//	int a,b;
+////	cin>>a>>b;
+////	cin>>str;
+//	cin>>a;
+//	cin>>str>>b;
+//	cout<<str<<"!!!"<<a+b<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//int main()
+//{
+//	float a,b;
+//	cin>>a>>b;
+//	cout<<a*b<<" "<<a/b<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//#include <string>
+//
+//using namespace std;
+//
+//int main()
+//{
+//	int a,b,c;
+//	
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//
+//using namespace std;
+//int main()
+//{
+//	float a=3.123456789;
+//	double b=3.123456789;
+//	printf("%-8.4f\n",a);//%n.mf--->n表示该浮点数最小宽度----m表示保留m位小数 
+//	printf("%8.3lf\n",b);//-号表示左对齐，宽度不足时右边补空格。没有符号则为右对齐，左边补空格
+//	printf("%08.3f\n",a);//宽度不足时补0 
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	int a;
+//	cin>>a;
+//	if(a>5)
+//	{
+//		
+//	}
+//	return 0;
+//} 
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//    int A,B,C,D;
+//    cin>>A;
+//    cin>>B;
+//    cin>>C;
+//    cin>>D;
+//    cout<<"DIFERENCA = "<<A*B-C*D<<endl;
+//    return 0;
+//}
+
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	double R;
+//	scanf("%lf",&R);
+//	printf("A=%.4lf",3.14159*R*R);//精度最好用double 更准确 
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	double A,B;
+//	cin>>A;
+//	cin>>B;
+//	printf("MEDIA = %.5lf",(A*3.5+B*7.5)/11);
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	int a,b;
+//	double c;
+//	cin>>a;
+//	cin>>b;
+//	cin>>c;
+//	cout<<"NUMBER = "<<a<<endl;
+//	printf("SALARY = U$ %.2lf",b*c);
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	int x;
+//	double y;
+//	cin>>x>>y;
+//	printf("%.3lf km/l",x/y);
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//	double x1,x2,y1,y2;
+//	cin>>x1>>y1>>x2>>y2;
+//	printf("%.4lf",sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1)));
+//	return 0;
+//}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
