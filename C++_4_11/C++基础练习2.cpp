e042ce000305f6575ba69d7574c0ba460e67b84d
bbbbbge <3101663227@qq.com>
2021-04-11 19:43:34 +0800

    C++基础练习2

---
+++ C++_4_11/C++基础练习2.cpp
@@ -0,0 +1,110 @@
+//#include <iostream>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//	cout<<floor(7.8)<<endl;
+//	cout<<ceil(7.8)<<endl;
+//	return 0;
+//} 
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int ans1;
+//	char ans2;
+//	ans1='M'-'A'+1;
+//	ans2='A'+18-1;
+//	cout<<ans1<<endl;
+//	cout<<ans2<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	char ch,ans;
+//	cin>>ch;
+//	ans=ch-'a'+'A';//输入小写输出 大写 
+//	cout<<ans<<endl;
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	int a;
+//	cin>>a;
+//	cout<<a<<'\n';
+//	cout<<a;
+//	return 0;
+//} 
+
+//#include <iostream>
+//#include <cstdio>
+//using namespace std;
+//int main()
+//{
+//	int a,b,c,d,time;
+//	cin>>a>>b>>c>>d;
+//	time=(c-a)*60+d-b;
+//	cout<<time/60<<" "<<time%60<<endl;//不能直接求经过分钟数和小时数，如果0.50—1.20--- 经过分钟数就是-30 
+//	return 0;
+//}
+//#include <iostream>
+//#include <cstdio>
+//#include <cmath>
+//using namespace std;
+//int main()
+//{
+//	int s,v,walk_time,out_time;
+//	cin>>s>>v;
+//	walk_time=ceil(1.0*s/v)+10;
+//	out_time=60*(24+8)-walk_time;
+//	printf("%02d:%02d",(out_time/60)%24,out_time%60);
+//	return 0;
+//}
+
+//#include <cstdio>
+//int main()
+//{
+//	float a=0.1;
+//	printf("%d\n",int(2-a*a*100));
+//	printf("%.7f",2-a*a*100);
+//	return 0;
+//}
+#include <cstdio>
+using namespace std;
+int main()
+{
+	printf("%d",int(0.1+0.2));
+	return 0;
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
