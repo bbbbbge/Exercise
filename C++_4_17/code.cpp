8f4a9a48305cb63cd3c390cbbc31d929d4ddbacb
bbbbbge <3101663227@qq.com>
2021-04-17 23:46:01 +0800

    PTA乙级练习1

---
+++ C++_4_17/code.cpp
@@ -0,0 +1,23 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+//#include <iostream>
+//using namespace std;
+//int main()
+//{
+//	int n;
+//	int count = 0;
+//	cin >> n;
+//	while (n != 1)
+//	{
+//		if (n % 2 == 0)
+//		{
+//			n /= 2;
+//		}
+//		else
+//		{
+//			n = (3 * n + 1) / 2;
+//		}
+//		count++;
+//	}
+//	cout << count << endl;
+//	return 0;
+//}
