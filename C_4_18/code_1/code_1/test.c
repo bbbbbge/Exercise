038813601387267d7879c32c0eb2174ce7aad0f8
bbbbbge <3101663227@qq.com>
2021-04-18 23:52:30 +0800

    C语言函数训练4

---
+++ C_4_18/code_1/code_1/test.c
@@ -0,0 +1,55 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+/*#include <stdio.h>
+void Swap(int* x, int* y)
+{
+	int tmp;
+	tmp = *x;
+	*x = *y;
+	*y = tmp;
+}
+int main()
+{
+	int a = 10;
+	int b = 20;
+	Swap(&a, &b);
+	printf("%d %d", a, b);
+	return 0;
+}*/
+
+//#include <stdio.h>
+//void print()
+//{
+//	printf("禁止套娃！");
+//	return 0;
+//}
+//int test()
+//{
+//	print();
+//	return 0;
+//}
+//int main()
+//{
+//	test();
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("%d\n", strlen("abc"));
+//}
+
+//#include <stdio.h>
+//#include <string.h>
+//int main()
+//{
+//	char arr1[20] = { 0 };
+//	char arr2[] = "Hello World";
+//
+//	//不用链式访问
+//	strcpy(arr1, arr2);
+//	printf("%s\n", arr1);
+//
+//	//链式访问
+//	printf("%s\n", strcpy(arr1, arr2);
+//}
