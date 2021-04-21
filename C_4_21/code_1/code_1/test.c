aed1c5ea6690f08f873e8deb6d33ab5d98a69bff
bbbbbge <3101663227@qq.com>
2021-04-21 23:36:47 +0800

    C函数练习6

---
+++ C_4_21/code_1/code_1/test.c
@@ -0,0 +1,17 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//ÇàÍÜÌøÌ¨½×
+#include <stdio.h>
+int step_way(int n)
+{
+	int count = 0;
+	if(n)
+}
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	int way=step_way(n);
+	printf("%d", way);
+	return 0;
+}
