a5b7209f3058db2b58232713ea916258f10d8cf4
bbbbbge <3101663227@qq.com>
2021-04-26 23:41:25 +0800

    C语言数组学习1

---
+++ C_4_26/code_1/code_1/test.c
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10];
+//	printf("%d", arr[7]);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char ch2[] = { 'a','b','c' };
+//	char ch4[] = "abc";
+//
+//	printf("%s\n", ch2);
+//	printf("%s\n", ch4);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+
+	return 0;
+}
