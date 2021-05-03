e10474e6733743b40f3acfdbe7dbe9943c78a1df
bbbbbge <3101663227@qq.com>
2021-05-03 23:34:04 +0800

    C++字符串训练5

---
+++ C++_5_3/code_1/code_1/test.cpp
@@ -0,0 +1,109 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+//int main()
+//{
+//	string s, a, b;
+//	getline(cin, s);
+//	cin >> a >> b;
+//	for (int i = 0; i < s.size(); i++)
+//	{
+//		int j = i;
+//		while (j < s.size() && s[j] != ' ') j++;
+//		if (a == s.substr(i, j)){
+//		
+//		else cout << s.substr(i, j) << ' ';
+//	}
+//	return 0;
+//}
+
+//#include <iostream>
+//using namespace std;
+//#include<ctime>
+//int calc(int x, int y, char ch)
+//{
+//	if (ch == '+') return x + y;
+//	if (ch == '-') return x - y;
+//	if (ch == '*') return x * y;
+//	if (ch == '/') return x / y;
+//	return 0;
+//}
+//int main(int a, int b, int c, int d)
+//{
+//	cout << "2020112966谢昕哲" << endl;
+//	cout << "请计算";
+//	srand((unsigned int)time(NULL));
+//	a = rand() % 10 + 1;
+//	b = rand() % 10 + 1;
+//	c = rand() % 4 + 1;
+//	int ans;
+//	switch (c)
+//	{
+//	case 1:cout << a << "+" << b << "=";
+//		ans = calc(a, b, '+');
+//		cin >> d;
+//		if (d != ans)
+//		{
+//			cout << "计算错误！";
+//			cout << a << "+" << b << "=" << ans;
+//		}
+//		else
+//		{
+//			cout << "计算正确！";
+//		}
+//		break;
+//	case 2:cout << a << "-" << b << "=";
+//		ans = calc(a, b, '-');
+//		cin >> d;
+//		if (d != ans)
+//		{
+//			cout << "计算错误！";
+//			cout << a << "-" << b << "=" << ans;
+//		}
+//		else
+//		{
+//			cout << "计算正确！";
+//		}
+//		break;
+//	case 3:cout << a << "*" << b << "=";
+//		ans = calc(a, b, '*');
+//		cin >> d;
+//		if (d != ans)
+//		{
+//			cout << "计算错误！";
+//			cout << a << "*" << b << "=" << ans;
+//		}
+//		else
+//		{
+//			cout << "计算正确！";
+//		}
+//		break;
+//	case 4:cout << a << "/" << b << "=";
+//		ans = calc(a, b, '/');
+//		cin >> d;
+//		if (d != ans)
+//		{
+//			cout << "计算错误！";
+//			cout << a << "/" << b << "=" << ans;
+//		}
+//		else
+//		{
+//			cout << "计算正确！";
+//		}
+//		break;
+//	}
+//	system("pause");
+//	return 0;
+//}
+
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+
+	return 0;
+}
