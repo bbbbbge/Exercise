c1dadbd5d1dcf7d6a2e29d9a12140fb8a162f39a
bbbbbge <3101663227@qq.com>
2021-05-28 23:13:25 +0800

    C语言划水的一天

---
+++ C_5_28/code_1/code_1/test.c
@@ -0,0 +1,44 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+//#include <stdio.h>
+//int main()
+//{
+//	/*char a = 3, b = 1;
+//	char c = a ^ b << 2;
+//	printf("%d", c);*/
+//	/*printf("a\bhow\'are\'y\\\bou\n");*/
+//	/*int a = 6;
+//	a += a -= a * a;
+//	printf("%d", a);*/
+//	/*char a='\\';
+//	printf("%c",a);*/
+//	int i = 100;
+//	for (;;)
+//	{
+//		i = i % 10 + 1;
+//		printf("hehe\n");
+//		if (i > 10)
+//			break;
+//	}
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//main()
+//{
+//	int k = 3;
+//	if (k)
+//	{
+//		printf("###");
+//	}
+//	else
+//		printf("&&&&");
+//}
+
+#include <stdio.h>
+int main()
+{
+
+	return 0;
+}
