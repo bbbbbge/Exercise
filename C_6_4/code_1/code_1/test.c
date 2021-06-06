a66777de2b09dd6cf8ceac35bca730e932b77d16
bbbbbge <3101663227@qq.com>
2021-06-06 13:35:03 +0800

    C语言指针进阶学习2

---
+++ C_6_4/code_1/code_1/test.c
@@ -0,0 +1,186 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+
+//#include <stdio.h>
+//int main()
+//{
+//	int arr[10][10] = { 0 };
+//	int i = 0;
+//	int j = 0;
+//	for (i = 0; i < 10; i++)
+//	{
+//		for (j = 0; j <= i; j++)
+//		{
+//			if (i == j || j == 0)
+//				arr[i][j] = 1;
+//			if (i >= 2 && j >= 1)
+//				arr[i][j] = arr[i - 1][j - 1] + arr[i - 1][j];
+//		}
+//	}
+//	for (i = 0; i < 10; i++)
+//	{
+//		
+//		for (j = 0; j <= i; j++)
+//		{
+//			printf("%d ", arr[i][j]);
+//		}
+//		printf("\n");
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char killer = 0;
+//	for (killer = 'A'; killer <= 'D'; killer++)
+//	{
+//		if ((killer != 'A') + (killer == 'C') + (killer == 'D') + (killer != 'D') == 3)
+//			printf("%c", killer);
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	int c = 0;
+//	int d = 0;
+//	int e = 0;
+//	for (a = 1; a <= 5; a++)
+//	{
+//		for (b = 1; b <= 5; b++)
+//		{
+//			for (c = 1; c <= 5; c++)
+//			{
+//				for (d = 1; d <= 5; d++)
+//				{
+//					for (e = 1; e <= 5; e++)
+//					{
+//						if ((b == 2) + (a == 3) == 1
+//							&& (b == 2) + (e == 4) == 1
+//							&& (c == 1) + (d == 2) == 1
+//							&& (c == 5) + (d == 3) == 1
+//							&& (e == 4) + (a == 1) == 1)
+//						{
+//							if (a*b*c*d*e == 120)
+//								printf("a=%d b=%d c=%d d=%d e=%d\n", a, b, c, d, e);
+//						}
+//					}
+//				}
+//			}
+//		}
+//	}
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void test(int arr[])//可以
+//{}
+//void test(int arr[10])//可以
+//{}
+//void test(int *arr)//可以，因为一维数组名就是数组首元素的地址
+//{}
+//void test2(int *arr[20])//可以
+//{}
+//void test2(int **arr)//不可以
+//{}
+//int main()
+//{
+//	int arr[10] = { 0 };//整型数组
+//	int *arr2[20] = { 0 };//指针数组
+//	test(arr);
+//	test2(arr2);
+//	return 0;
+//}////#include <stdio.h>
+//void test(int(*arr)[5], int row, int col)//ok？
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < col; j++)
+//		{
+//			printf("%d ", *(*(arr + i) + j));
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { 1,2,3,4,5,6,7,8,9,10,11,12,13,14,15 };
+//	
+//	test(arr, 3, 5);
+//	
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void print2(int(*p)[5], int row, int col)
+//{
+//	int i = 0;
+//	for (i = 0; i < row; i++)
+//	{
+//		int j = 0;
+//		for (j = 0; j < col; j++)
+//		{
+//			printf("%d ", *(*(p + i) + j));
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int arr[3][5] = { {1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15} };
+//	print2(arr, 3, 5);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void print(int *p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n", *(p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 1,2,3,4,5,6,7,8,9 };
+//	int *p = arr;
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//一级指针p，传给函数
+//	print(p, sz);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//void test(int** ptr)
+//{
+//	printf("num = %d\n", **ptr);
+//}
+//int main()
+//{
+//	int n = 10;
+//	int*p = &n;
+//	int **pp = &p;
+//	test(pp);
+//	test(&p);
+//	return 0;
+//}
+
+#include <stdio.h>
+int main()
+{
+	int arr[5] = { 1,2,3,4,5 };
+	int *p = arr;
+	int i = 0;
+	for (i = 0; i < 5; i++)
+		printf("%d ", p[i]);
+}
