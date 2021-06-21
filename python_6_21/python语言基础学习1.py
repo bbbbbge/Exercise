9b29dd22bdeefb8b88d1cb674c4c4a1bf1d45928
bbbbbge <3101663227@qq.com>
2021-06-21 23:52:42 +0800

    python语言基础学习1

---
+++ python_6_21/python语言基础学习1.py
@@ -0,0 +1,104 @@
+
+'''
+x=10
+y=3.14
+z=1+2j
+print(type(x), type(y), type(z))
+
+'''
+# print(type(10>5))
+'''
+y=False
+print(y+1)
+'''
+'''
+a="jfkldsakdsflsa"
+print(type(a))
+'''
+'''
+x=3.457; y=10
+# print("x=%.2f y=%d"%(x,y))
+print("x={1:.2f} y={0}".format(x,y))
+print("y={0} x={1}".format(x,y))
+'''
+# help('keywords')
+
+'''
+a=2; b=3
+#a=a**b
+#print(a)
+print(b/a)
+print(b//a)'''
+'''
+x=True
+y=False
+print(x and y)
+print(x or y)
+print(not x)
+'''
+'''
+s='fadsjlf'
+print('f' in s)
+print('F' in s)
+
+num=input()
+print(int(num)+20)
+
+a,b,c=10,20,30
+print(a,b,c)
+print(a);print(b);print(c)
+print(a,b,c,sep=",")
+print(a,end="");print(b,end="");print(c,end="a")
+
+c=max(2,10)
+print(c)
+a=divmod(9,2)
+print(a)
+b=abs(-7)
+print(b)
+c=sum([1,2,3,4])
+print(c)
+
+import math
+print(math.pi)
+print(math.sin(math.pi/2))
+
+import math as m
+print(m.pi)
+
+from math import *
+print(pi)
+
+from math import pi,sin
+print(pi)
+'''
+'''
+import random
+
+print(random.random())
+print(random.random())
+print(random.random())
+'''
+'''
+import time
+t=time.localtime()
+t1=time.strftime("%Y-%m-%d %H:%M;%S",t)
+print(t1)
+'''
+
+import datetime
+d=datetime.datetime(2019,5,1)
+#from datetime import datetime
+#c=datetime(2019,5,2)
+#print(c)
+print(d)
+a=datetime.datetime.today()
+print(a)
+
+
+
+
+
+
+
+
